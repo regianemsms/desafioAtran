37d00c1d011a476da90301c555d72827f0ff3806
Regiane Mesquita <regianemsms@gmail.com/>
2019-11-27 22:50:10 -0300

    adicionando projeto ao git

---
+++ CarrinhoDeComprasApi/src/main/java/com/altran/CarrinhoDeComprasApi/CarrinhoDeComprasApiApplication.java
@@ -0,0 +1,13 @@
+package com.altran.CarrinhoDeComprasApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrinhoDeComprasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrinhoDeComprasApiApplication.class, args);
+	}
+
+}
